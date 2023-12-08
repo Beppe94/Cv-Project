238c9afb77eac6fc18003a28e6bc5f2ae3f30bce
Beppe <giuseppe.monorchio94@hotmail.it>
2023-12-08 16:40:57 +0100

    add fontawesome icons

--- src/Components/expandSection.jsx
+++ src/Components/expandSection.jsx
@@ -1,13 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 function ExpandSection({
     isOpen, 
     setOpen, 
     sectionName}) {
 
+    const up = <FontAwesomeIcon icon={faChevronUp} />
+    const down = <FontAwesomeIcon icon={faChevronDown} />
+
     return (
         <button
         onClick={() => setOpen(isOpen ? 'open' : sectionName)}
         >
-            {sectionName} ^
+            {sectionName} 
+            <i>{isOpen ? down : up}</i>
         </button>
     );
 }
